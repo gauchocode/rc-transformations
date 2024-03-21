373303a8138ed299d03e9096a8513c6b95cdd5c1
Franco Liza <fliza@gauchocode.com>
2024-03-21 13:12:59 -0300

    fix query

--- models/paypal/paypal_raw_transactions_dbt.sql
+++ models/paypal/paypal_raw_transactions_dbt.sql
@@ -13,7 +13,5 @@ select
     pt.transaction_info->'fee_amount'->>'value'				as fee_amount,
     pt.transaction_info->'fee_amount'->>'currency_code'		as fee_currency,
     pt.transaction_info->'custom_field'						as custom_field,
-    pt.transaction_info->'paypal_account_id'				as customer_id    transaction_info->>'invoice_id'             as invoice_id,
-    transaction_id                              as transaction_id,
-    pt.transaction_info->>'paypal_reference_id' as paypal_reference_id
+    pt.transaction_info->'paypal_account_id'				as customer_id
 from pp_transactions;
