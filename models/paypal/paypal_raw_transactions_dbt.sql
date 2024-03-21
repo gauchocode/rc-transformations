425847870e921bf4ac0f4a65b4ee709af9ecc3f9
Franco Liza <fliza@gauchocode.com>
2024-03-21 15:28:36 -0300

    with clouse experiment

--- models/paypal/paypal_raw_transactions_dbt.sql
+++ models/paypal/paypal_raw_transactions_dbt.sql
@@ -1,18 +1,24 @@
 {{ config(materialized="table" ) }}
 
-select 
-    SB                                                                  as ch,
-    transaction_id                                                      as transaction_id,
-    transaction_info->>'invoice_id'                                     as invoice_id,
-    transaction_info->>'paypal_reference_id'                            as paypal_reference_id,
-    transaction_info->>'paypal_reference_id_type'                       as paypal_reference_id_type,
-    transaction_info->>'transaction_event_code'                         as transaction_event_code,
-    transaction_info->>'transaction_initiation_date'                    as transaction_initiation_date,
-    transaction_info->>'transaction_updated_date'                       as transaction_completion_date,
-    CAST(transaction_info->'transaction_amount'->>'value' as FLOAT)     as gross_transaction_amount,
-    transaction_info->'transaction_amount'->>'currency_code'            as gross_transaction_currency,
-    -CAST(transaction_info->'fee_amount'->>'value' as FLOAT)            as fee_amount,
-    transaction_info->'fee_amount'->>'currency_code'                    as fee_currency,
-    transaction_info->'custom_field'                                    as custom_field,
-    transaction_info->'paypal_account_id'                               as customer_id
-from pp_transactions
+with transactions as (
+
+    select 
+        SB                                                                  as ch,
+        transaction_id                                                      as transaction_id,
+        transaction_info->>'invoice_id'                                     as invoice_id,
+        transaction_info->>'paypal_reference_id'                            as paypal_reference_id,
+        transaction_info->>'paypal_reference_id_type'                       as paypal_reference_id_type,
+        transaction_info->>'transaction_event_code'                         as transaction_event_code,
+        transaction_info->>'transaction_initiation_date'                    as transaction_initiation_date,
+        transaction_info->>'transaction_updated_date'                       as transaction_completion_date,
+        CAST(transaction_info->'transaction_amount'->>'value' as FLOAT)     as gross_transaction_amount,
+        transaction_info->'transaction_amount'->>'currency_code'            as gross_transaction_currency,
+        -CAST(transaction_info->'fee_amount'->>'value' as FLOAT)            as fee_amount,
+        transaction_info->'fee_amount'->>'currency_code'                    as fee_currency,
+        transaction_info->'custom_field'                                    as custom_field,
+        transaction_info->'paypal_account_id'                               as customer_id
+    from pp_transactions
+)
+
+select *
+from transactions
