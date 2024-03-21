2978f0690feab0bc6c90e5eabc1f10d93f8839af
Franco Liza <fliza@gauchocode.com>
2024-03-21 00:51:04 -0300

    it's working

--- models/paypal/paypal_raw_transactions_dbt.sql
+++ models/paypal/paypal_raw_transactions_dbt.sql
@@ -1,9 +1,14 @@
 {{ config(materialized="table" ) }}
 
+with payer_info_json as (
+    select 
+        payer_info->>'account_id' as account_id
+    from pp_transactions
+)
+
 select 
     (
-        select payer_info->'account_id'
-        from transaccion
-    ) as account_id
-    "transaction_id"            as transaccion
-from transactions
+        select * from payer_info_json
+    )
+    transaction_id            as transaccion
+from pp_transactions;
