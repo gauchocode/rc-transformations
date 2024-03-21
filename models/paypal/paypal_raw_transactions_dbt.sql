dd869ace1bbbacff8339b49a1f6c7c2b7e1c93af
Franco Liza <fliza@gauchocode.com>
2024-03-21 01:11:21 -0300

    esto va pipi cucu

--- models/paypal/paypal_raw_transactions_dbt.sql
+++ models/paypal/paypal_raw_transactions_dbt.sql
@@ -1,14 +1,6 @@
 {{ config(materialized="table" ) }}
 
-with payer_info_json as (
-    select 
-        payer_info->>'account_id' as account_id
-    from pp_transactions
-)
-
 select 
-    (
-        select * from payer_info_json
-    ),
+    payer_info->>'account_id' as account_id,
     transaction_id            as transaccion
 from pp_transactions;
