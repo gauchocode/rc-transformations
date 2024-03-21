d316d4b2aa727da82f1dde48a2151c2ff46f01fa
Franco Liza <fliza@gauchocode.com>
2024-03-20 23:19:32 -0300

    fix models

--- models/paypal/paypal_settlement_dbt.sql
+++ models/paypal/paypal_settlement_dbt.sql
@@ -1,9 +1,5 @@
 {{ config(materialized="table" ) }}
 
 select 
-    (
-        select payer_info->>'account_id' as account_id
-        from paypal_raw_transactions
-    ) as transaction_id
-
+    "payer_info->>'account_id'" as account_id
 from paypal_raw_transactions
