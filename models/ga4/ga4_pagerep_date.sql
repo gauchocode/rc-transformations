011b5820ea217971d2fa03bba22c4106af8b9424
Franco Liza <fliza@gauchocode.com>
2024-03-22 17:54:17 -0300

    adding transactions column

--- models/ga4/ga4_pagerep_date.sql
+++ models/ga4/ga4_pagerep_date.sql
@@ -1,10 +1,11 @@
 {{ config(materialized="table" ) }}
 
 select 
-    CAST("date" as DATE)                        as date,
-    SUM(CAST("newUsers" as INT))                as new_users,
-    SUM(CAST("totalRevenue" as INT))            as total_revenue,
-    SUM(CAST("userEngagementDuration" as INT))  as user_engagement_duration
+    CAST("date"                         as DATE)    as date,
+    SUM(CAST("userEngagementDuration"   as INT))    as user_engagement_duration,
+    SUM(CAST("conversions"              as INT))    as transactions,
+    SUM(CAST("newUsers"                 as INT))    as new_users,
+    SUM(CAST("totalRevenue"             as INT))    as total_revenue
 from 
     social.ga4_pages_path_report
 group by
