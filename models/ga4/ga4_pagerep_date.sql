97c4a179d497ed05fab131d4d98ae37136543374
Franco Liza <fliza@gauchocode.com>
2024-03-22 17:17:56 -0300

    adding new_users column

--- models/ga4/ga4_pagerep_date.sql
+++ models/ga4/ga4_pagerep_date.sql
@@ -1,8 +1,9 @@
 {{ config(materialized="table" ) }}
 
 select 
-    "date" as date,
-    SUM(CAST(userEngagementDuration as INT)) as user_engagement_duration
+    "date"                                      as date,
+    SUM(CAST("newUsers") as INT)                as new_users
+    SUM(CAST("userEngagementDuration" as INT))  as user_engagement_duration
 from 
     social.ga4_pages_path_report
 group by
