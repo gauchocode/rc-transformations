921234b6270b49166f353817a70a6d57ee08f1dd
Franco Liza <fliza@gauchocode.com>
2024-03-22 16:41:11 -0300

    adding social schema to ga4

--- models/ga4/ga4_pagerep_date.sql
+++ models/ga4/ga4_pagerep_date.sql
@@ -4,6 +4,6 @@ select
     'date' as date,
     SUM(CAST(userEngagementDuration as INT)) as user_engagement_duration
 from 
-    ga4_pages_path_report
+    social.ga4_pages_path_report
 group by
     'date'
