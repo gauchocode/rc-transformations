d38f75c02deba7c9522932b3e0fa9a1459956995
Franco Liza <fliza@gauchocode.com>
2024-03-22 18:09:31 -0300

    adding session column

--- models/ga4/ga4_pagerep_date.sql
+++ models/ga4/ga4_pagerep_date.sql
@@ -3,10 +3,11 @@
 select 
     CAST("date"                         as DATE)    as date,
     SUM(CAST("userEngagementDuration"   as INT))    as user_engagement_duration,
-    SUM(CAST("conversions"              as INT))    as transactions,
+    SUM(CAST("engagedSessions"          as INT))    as sessions,
     SUM(CAST("newUsers"                 as INT))    as new_users,
+    SUM(CAST("conversions"              as INT))    as transactions,
     SUM(CAST("totalRevenue"             as INT))    as total_revenue
 from 
-    social.ga4_pages_path_report
+    social.ga4_user_acquisition_first_user_campaign_report
 group by
     "date"
