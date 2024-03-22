b51532b88dd49b4233f13ac6eeecff61a30894f8
Franco Liza <fliza@gauchocode.com>
2024-03-22 17:37:09 -0300

    add missing comma

--- models/ga4/ga4_pagerep_date.sql
+++ models/ga4/ga4_pagerep_date.sql
@@ -2,7 +2,7 @@
 
 select 
     "date"                                      as date,
-    SUM(CAST("newUsers") as INT)                as new_users
+    SUM(CAST("newUsers") as INT)                as new_users,
     SUM(CAST("userEngagementDuration" as INT))  as user_engagement_duration
 from 
     social.ga4_pages_path_report
