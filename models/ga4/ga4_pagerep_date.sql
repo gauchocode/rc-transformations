135b8267d9b53309f31d1d4f1de66b9edd400224
Franco Liza <fliza@gauchocode.com>
2024-03-22 16:56:22 -0300

    small changes

--- models/ga4/ga4_pagerep_date.sql
+++ models/ga4/ga4_pagerep_date.sql
@@ -1,9 +1,9 @@
 {{ config(materialized="table" ) }}
 
 select 
-    'date' as date,
+    "date" as date,
     SUM(CAST(userEngagementDuration as INT)) as user_engagement_duration
 from 
     social.ga4_pages_path_report
 group by
-    'date'
+    "date"
