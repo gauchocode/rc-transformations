4de64bf5818f1c7538d82e4c3186476bcff206bb
Franco Liza <fliza@gauchocode.com>
2024-03-25 17:35:42 -0300

    fix

--- models/turnit/turnit_trip_route_info.sql
+++ models/turnit/turnit_trip_route_info.sql
@@ -1,19 +1,19 @@
 {{ config(materialized="table") }}
 
 select
-    "id"                                        as id,
-    "TripId"                                    as trip_id,
-    "CreateAt"                                  as create_at,
-    "StopCode"                                  as stop_code,
-    "UpdateAt"                                  as update_at,
-    "RouteOrder"                                as route_order,
-    "PlatformName"                              as platform_name,
-    "StopTimezone"                              as stop_timezone,
-    "StopTypeCode"                              as stop_type_code,
-    "ActualArrivalTime"                         as actual_arrival_time,
-    "PlannedArrivalTime"                        as planned_arrival_time,
-    "ActualDepartureTime"                       as actual_departure_time,
-    "PlannedDepartureTime"                      as planned_departure_time,
-    COALESCE("DistanceFromPreviousStop", '')    as distance_from_previous_stop
+    "id"                                    as id,
+    "TripId"                                as trip_id,
+    "CreateAt"                              as create_at,
+    "StopCode"                              as stop_code,
+    "UpdateAt"                              as update_at,
+    "RouteOrder"                            as route_order,
+    "PlatformName"                          as platform_name,
+    "StopTimezone"                          as stop_timezone,
+    "StopTypeCode"                          as stop_type_code,
+    "ActualArrivalTime"                     as actual_arrival_time,
+    "PlannedArrivalTime"                    as planned_arrival_time,
+    "ActualDepartureTime"                   as actual_departure_time,
+    "PlannedDepartureTime"                  as planned_departure_time,
+    COALESCE("DistanceFromPreviousStop", 0) as distance_from_previous_stop
 
 from public.turnit_trip_route_info
