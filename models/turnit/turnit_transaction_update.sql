4d517f0fe7e95e733151ed8b1c6396970be90279
Franco Liza <fliza@gauchocode.com>
2024-03-25 16:39:56 -0300

    fix

--- models/turnit/turnit_transaction_update.sql
+++ models/turnit/turnit_transaction_update.sql
@@ -8,8 +8,8 @@ select
     "NewTrip_ID"        as new_trip_id,
     "OldTrip_ID"        as old_trip_id,
     "TransactionId"     as Transaction_id,
-    "NewTrip_bus_ID"    as new_trip_bus_id
-
-
+    "NewTrip_bus_ID"    as new_trip_bus_id,
+    "OldTrip_bus_ID"    as old_trip_bus_id,
+    "TransactionCode"   as Transaction_code
 
 from public.turnit_transaction_update
