a7eaddecc72a360b76287475dae2f0ba47e8862d
Franco Liza <fliza@gauchocode.com>
2024-03-25 13:03:01 -0300

    upload turnit_loyalty transformation

---
+++ models/turnit/turnit_loyalty.sql
@@ -0,0 +1,13 @@
+{{ config(materialized="table" ) }}
+
+select
+"id"                        as id,
+"CreateAt"                  as create_at,
+"Distance"                  as distance,
+"UpdateAt"                  as update_at,
+"OriginStopId"              as origin_stop_id,
+"LineTemplateId"            as line_template_id,
+"DestinationStopId"         as destination_stop_id,
+"OriginOrderNumber"         as origin_order_number,
+"DestinationOrderNumber"    as destination_order_number
+from
