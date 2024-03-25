f131f8f20755fccfb273db2bf3a99429cf3b373b
Franco Liza <fliza@gauchocode.com>
2024-03-25 12:58:11 -0300

    upload turnit_line_template_distance transformation

---
+++ models/turnit/turnit_line_template_distance.sql
@@ -0,0 +1,14 @@
+{{ config(materialized="table" ) }}
+
+select 
+    "id"                        as id,
+    "CreateAt"                  as create_at,
+    "Distance"                  as distance,
+    "UpdateAt"                  as update_at,
+    "OriginStopId"              as origin_stop_id,
+    "LineTemplateId"            as line_template_id,
+    "DestinationStopId"         as destination_stop_id,
+    "OriginOrderNumber"         as origin_order_number,
+    "DestinationOrderNumber"    as destination_order_number
+    
+from public.turnit_line_template_distance
