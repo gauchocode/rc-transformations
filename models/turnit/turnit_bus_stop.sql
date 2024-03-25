6182aabc8a168c10bea6e639383f9df0719b7625
Franco Liza <fliza@gauchocode.com>
2024-03-25 11:57:44 -0300

    update in turnit_bus_stop model

--- models/turnit/turnit_bus_stop.sql
+++ models/turnit/turnit_bus_stop.sql
@@ -1,24 +1,24 @@
 {{ config(materialized="table" ) }}
 
 select
-    "Id"                as id,
-    "Zip"               as zip,
-    "Code"              as code,
-    "Name"              as name,
-    "CityName"          as city_name,
-    "CreateAt"          as create_at,
-    "Latitude"          as latitude,
-    "Timezone"          as timezone,
-    "UpdateAt"          as update_at,
-    "Longitude"         as longitude,
-    "CountryCode"       as country_code,
-    "CountryName"       as country_name,
-    "HasPriority"       as has_priotity,
-    "PlatformName"      as platform_name,
-    "IsMultiLegHub"     as is_multi_leg_hub,
-    "StreetAddress"     as street_address,
-    "PlatformLatitude"  as platform_latitude,
-    "MarketingCityName" as marketing_city_name,
-    "PlatformLongitude" as platform_longitude
+    "Id"                                as id,
+    "Zip"                               as zip,
+    "Code"                              as code,
+    "Name"                              as name,
+    "CityName"                          as city_name,
+    "CreateAt"                          as create_at,
+    "Latitude"                          as latitude,
+    "Timezone"                          as timezone,
+    "UpdateAt"                          as update_at,
+    "Longitude"                         as longitude,
+    "CountryCode"                       as country_code,
+    "CountryName"                       as country_name,
+    "HasPriority"                       as has_priotity,
+    COALESCE("PlatformName", '')        as platform_name,
+    "IsMultiLegHub"                     as is_multi_leg_hub,
+    "StreetAddress"                     as street_address,
+    "PlatformLatitude"                  as platform_latitude,
+    "MarketingCityName"                 as marketing_city_name,
+    COALESCE("PlatformLongitude", '')   as platform_longitude
 from public.turnit_bus_stop
 
