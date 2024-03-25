2c0937290818ef69acd651575e4d9f5a17359840
Franco Liza <fliza@gauchocode.com>
2024-03-25 16:40:37 -0300

    fix 2

--- models/turnit/turnit_transaction_move.sql
+++ models/turnit/turnit_transaction_move.sql
@@ -8,6 +8,6 @@ select
     "CreatedOn"         as created_on,
     "NewTrip_bus_ID"    as new_trip_bus_id,
     "OldTrip_bus_ID"    as old_trip_bus_id,
-    "TransactionCode"   as transaction_code,
+    "TransactionCode"   as transaction_code
 
 from public.turnit_transaction_move
