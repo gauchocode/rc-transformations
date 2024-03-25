5f5f571232c3d0532eb561f4e2f2ac167e087bd4
Franco Liza <fliza@gauchocode.com>
2024-03-25 14:40:44 -0300

    fix

--- models/turnit/turnit_loyalty.sql
+++ models/turnit/turnit_loyalty.sql
@@ -2,13 +2,20 @@
 
 select
     "id"                        as id,
+    COALESCE("Carbon", '')      as carbon,
+    "Points"                    as points,
+    "Reason"                    as reason,
+    "Status"                    as status,
+    "Assignee"                  as assignee,
     "CreateAt"                  as create_at,
-    "Distance"                  as distance,
+    COALESCE("Distance", 0)     as distance,
     "UpdateAt"                  as update_at,
-    "OriginStopId"              as origin_stop_id,
-    "LineTemplateId"            as line_template_id,
-    "DestinationStopId"         as destination_stop_id,
-    "OriginOrderNumber"         as origin_order_number,
-    "DestinationOrderNumber"    as destination_order_number
+    "ChangedOn"                 as changed_on,
+    "CreatedOn"                 as created_on,
+    "ValidFrom"                 as valid_from,
+    "ValidUntil"                as valid_until,
+    "VoucherNumber"             as voucher_number,
+    "TransactionCode"           as transaction_code,
+    "TravelAccountId"           as travel_account_id,
 
 from public.turnit_loyalty
