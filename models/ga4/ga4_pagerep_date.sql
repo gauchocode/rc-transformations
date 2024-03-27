9af560cc213dcc663870b4a2e05fa480742b7780
Franco Liza <fliza@gauchocode.com>
2024-03-26 21:05:56 -0300

    fix

--- models/ga4/ga4_pagerep_date.sql
+++ models/ga4/ga4_pagerep_date.sql
@@ -1,12 +1,12 @@
 {{ config(materialized="table" ) }}
 
 select 
-	CAST(date as DATE)				as date
-	userEngagementDuration			as user_engagement_duration
-	sessions						as sessions
-	newUsers						as new_users
-	transactions					as transactions
-	totalRevenue					as total_revenue
+	CAST(date as DATE)				as date,
+	userEngagementDuration			as user_engagement_duration,
+	sessions						as sessions,
+	newUsers						as new_users,
+	transactions					as transactions,
+	totalRevenue					as total_revenue,
 	engagedSessions					as engaged_sessions
 
-from ga4_pagerep_date
+from public.ga4_pagerep_date
