44894d4a84cd9a439d99dc75af27b5464c3a6bac
Franco Liza <fliza@gauchocode.com>
2024-03-27 11:22:44 -0300

    fix

--- models/paypal/paypal_settlement.sql
+++ models/paypal/paypal_settlement.sql
@@ -9,7 +9,7 @@ select
     CAST(transaction_info->'transaction_event_code' as TEXT)                                        as transaction_event_code,
     to_timestamp(transaction_info->>'transaction_initiation_date', 'YYYY-MM-DD"T"HH24:MI:SSOF')     as transaction_initiation_date,
     to_timestamp(transaction_info->>'transaction_updated_date', 'YYYY-MM-DD"T"HH24:MI:SSOF')        as transaction_completion_date,
-    ABS(COALESCE(CAST(transaction_info->>'transaction_amount'->>'value' as FLOAT), 0))              as gross_transaction_amount,
+    ABS(COALESCE(CAST(transaction_info->>'transaction_amount'->'value' as FLOAT), 0))              as gross_transaction_amount,
     COALESCE(transaction_info->'transaction_amount'->'currency_code', '')                           as gross_transaction_currency,
     ABS(COALESCE(CAST(transaction_info->'fee_amount'->>'value' as FLOAT), 0))                      as fee_amount,
     COALESCE(transaction_info->'fee_amount'->>'currency_code', '')                                  as fee_currency,
