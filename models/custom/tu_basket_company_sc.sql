a4fd137d094bb19dbcb9d78ceb5c738bc2b9197f
Franco Liza <fliza@gauchocode.com>
2024-05-23 14:27:38 -0300

    update custom model

--- models/custom/tu_basket_company_sc.sql
+++ models/custom/tu_basket_company_sc.sql
@@ -2,24 +2,20 @@
 
 with
        transbordo as (
-       select distinct product_code
+       select distinct basket_code
        from public.turnit_product_activity_view
-       where leg_order = 2 --type = 'BUS_TICKET' and operation_group = 1 and operation_user <> '888' and
+       where leg_order = 2
        )
 
-SELECT p.basket_code,
-max(case
-       when transbordo.product_code is not null then 'Atlanta'
-       else coalesce(r.company, 'Others')
-end) as company_sc
-   
-
+select p.basket_code,
+case
+       when max(transbordo.basket_code) is not null then 'Atlanta'
+       else coalesce(max(r.company), 'Others')
+end as company_sc
+ 
 FROM public.turnit_product_activity_view p
-
 left join public.turnit_trip t on p.trip_id = t.trip_id
 left join public.maestros_ma_rutas r on cast(r.service_code as int) = cast(t.trip_service_code as int)
-left join transbordo on transbordo.product_code = p.product_code
+left join transbordo on transbordo.basket_code = p.basket_code
 
- --where p.type = 'BUS_TICKET' and p.operation_group = 1 and p.operation_user <> '888'  and
-
- group by basket_code order by company_sc
+group by p.basket_code
