633b59f92664e2376109309ce2c1e4501b3513da
Franco Liza <fliza@gauchocode.com>
2024-06-11 20:32:58 -0300

    cambios en la macro

--- macros/update_origin_and_destination_codes.sql
+++ macros/update_origin_and_destination_codes.sql
@@ -1,25 +1,25 @@
-{% macro update_columns() %}
+{% macro update_origin_and_destination_codes() %}
 -- Actualiza columnas en la tabla turnit_product_activity_view
-UPDATE {{ ref('turnit_product_activity_view') }}
+UPDATE {{ ref('public.turnit_product_activity_view') }}
 SET
-    journey_origin_stop_code = CASE 
-        WHEN journey_origin_stop_code = 'UCL' THEN 'UFL'
-        WHEN journey_origin_stop_code = 'FMP' THEN 'FM'
-        ELSE journey_origin_stop_code
+    public.journey_origin_stop_code = CASE 
+        WHEN public.journey_origin_stop_code = 'UCL' THEN 'UFL'
+        WHEN public.journey_origin_stop_code = 'FMP' THEN 'FM'
+        ELSE public.journey_origin_stop_code
     END,
-    journey_destination_stop_code = CASE 
-        WHEN journey_destination_stop_code = 'UCL' THEN 'UFL'
-        WHEN journey_destination_stop_code = 'FMP' THEN 'FM'
-        ELSE journey_destination_stop_code
+    public.journey_destination_stop_code = CASE 
+        WHEN public.journey_destination_stop_code = 'UCL' THEN 'UFL'
+        WHEN public.journey_destination_stop_code = 'FMP' THEN 'FM'
+        ELSE public.journey_destination_stop_code
     END,
-    origin_stop_code = CASE 
-        WHEN origin_stop_code = 'UCL' THEN 'UFL'
-        WHEN origin_stop_code = 'FMP' THEN 'FM'
-        ELSE origin_stop_code
+    public.origin_stop_code = CASE 
+        WHEN public.origin_stop_code = 'UCL' THEN 'UFL'
+        WHEN public.origin_stop_code = 'FMP' THEN 'FM'
+        ELSE public.origin_stop_code
     END,
-    destination_stop_code = CASE 
-        WHEN destination_stop_code = 'UCL' THEN 'UFL'
-        WHEN destination_stop_code = 'FMP' THEN 'FM'
-        ELSE destination_stop_code
+    public.destination_stop_code = CASE 
+        WHEN public.destination_stop_code = 'UCL' THEN 'UFL'
+        WHEN public.destination_stop_code = 'FMP' THEN 'FM'
+        ELSE public.destination_stop_code
     END;
 {% endmacro %}
