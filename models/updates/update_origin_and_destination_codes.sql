bd8271fb53752adcf6d8daf2ff325fe6cbdd351b
Franco Liza <fliza@gauchocode.com>
2024-06-11 21:45:53 -0300

    fix

--- models/updates/update_origin_and_destination_codes.sql
+++ models/updates/update_origin_and_destination_codes.sql
@@ -1,6 +1,44 @@
-{{ config(
-    materialized = 'table'  -- O 'ephemeral' o cualquier materialización adecuada
-) }}
-
--- Llamar a la macro de actualización
-{% do update_stop_codes() %}
+UPDATE turnit_product_activity_view
+SET
+    journey_origin_stop_code = CASE 
+        WHEN journey_origin_stop_code = 'UCL' THEN 'UFL'
+        WHEN journey_origin_stop_code = 'FMP' THEN 'FM'
+        ELSE journey_origin_stop_code
+    END,
+    journey_destination_stop_code = CASE 
+        WHEN journey_destination_stop_code = 'UCL' THEN 'UFL'
+        WHEN journey_destination_stop_code = 'FMP' THEN 'FM'
+        ELSE journey_destination_stop_code
+    END,
+    origin_stop_code = CASE 
+        WHEN origin_stop_code = 'UCL' THEN 'UFL'
+        WHEN origin_stop_code = 'FMP' THEN 'FM'
+        ELSE origin_stop_code
+    END,
+    destination_stop_code = CASE 
+        WHEN destination_stop_code = 'UCL' THEN 'UFL'
+        WHEN destination_stop_code = 'FMP' THEN 'FM'
+        ELSE destination_stop_code
+    END;
+UPDATE turnit_product_activity_view
+SET
+    journey_origin_stop_code = CASE 
+        WHEN journey_origin_stop_code = 'UCL' THEN 'UFL'
+        WHEN journey_origin_stop_code = 'FMP' THEN 'FM'
+        ELSE journey_origin_stop_code
+    END,
+    journey_destination_stop_code = CASE 
+        WHEN journey_destination_stop_code = 'UCL' THEN 'UFL'
+        WHEN journey_destination_stop_code = 'FMP' THEN 'FM'
+        ELSE journey_destination_stop_code
+    END,
+    origin_stop_code = CASE 
+        WHEN origin_stop_code = 'UCL' THEN 'UFL'
+        WHEN origin_stop_code = 'FMP' THEN 'FM'
+        ELSE origin_stop_code
+    END,
+    destination_stop_code = CASE 
+        WHEN destination_stop_code = 'UCL' THEN 'UFL'
+        WHEN destination_stop_code = 'FMP' THEN 'FM'
+        ELSE destination_stop_code
+    END;
