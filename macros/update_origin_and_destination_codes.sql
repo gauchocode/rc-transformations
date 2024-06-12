b0f09bdeb41a77a1fadb836cd2fa22e5f5541956
Franco Liza <fliza@gauchocode.com>
2024-06-11 21:44:13 -0300

    update script

--- macros/update_origin_and_destination_codes.sql
+++ macros/update_origin_and_destination_codes.sql
@@ -1,6 +1,4 @@
-{% macro update_stop_codes() %}
--- Actualiza columnas en la tabla turnit_product_activity_view
-UPDATE {{ ref('turnit_product_activity_view') }}
+UPDATE turnit_product_activity_view
 SET
     journey_origin_stop_code = CASE 
         WHEN journey_origin_stop_code = 'UCL' THEN 'UFL'
@@ -22,4 +20,3 @@ SET
         WHEN destination_stop_code = 'FMP' THEN 'FM'
         ELSE destination_stop_code
     END;
-{% endmacro %}
