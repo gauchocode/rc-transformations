6514eb3db4b292f9dc39e2d944efa2431f80bd9d
Franco Liza <fliza@gauchocode.com>
2024-09-10 22:37:22 -0300

    fix

--- models/business_case/business_case.sql
+++ models/business_case/business_case.sql
@@ -6,5 +6,5 @@ select
     "TktsNew"                                                                       as TktsNew,
     CAST(REPLACE(REPLACE(Avg_Tkt_Fee, '$', ''), ',', '') as DECIMAL(10, 2))         as Avg_Tkt_Fee,
     CAST(REPLACE(REPLACE(Ingreso_Real, '$', ''), ',', '') as DECIMAL(10, 2))        as Ingreso_Real,
-    CAST(REPLACE(REPLACE(Ingreso_Tkt_Fee, '$', ''), ',', '') as DECIMAL(10, 2))     as Ingreso_Tkt_Fee,
+    CAST(REPLACE(REPLACE(Ingreso_Tkt_Fee, '$', ''), ',', '') as DECIMAL(10, 2))     as Ingreso_Tkt_Fee
 from public.business_case
