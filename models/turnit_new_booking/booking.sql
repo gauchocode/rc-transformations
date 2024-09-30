b93ef4f0801b9084c9ce5376f8e90bef310fcdbc
Franco Liza <fliza@gauchocode.com>
2024-09-30 18:10:42 -0300

    minor changes

--- models/turnit_new_booking/booking.sql
+++ models/turnit_new_booking/booking.sql
@@ -9,4 +9,4 @@ SELECT
     changed_by_user_id                                                          as changed_by_user_id,
     created_by_user_id                                                          as row_updated_on_utc
 
-FROM bookings.booking
+FROM booking
