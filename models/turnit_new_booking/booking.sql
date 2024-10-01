12f05296705b7bc188b7c3ee6ecdaa15c250dcd5
Franco Liza <fliza@gauchocode.com>
2024-10-01 10:14:00 -0300

    update booking transformation

--- models/turnit_new_booking/booking.sql
+++ models/turnit_new_booking/booking.sql
@@ -7,6 +7,10 @@ SELECT
     changed_on_utc                                                              as changed_on_utc,
     changed_on_utc                                                              as created_on_utc,
     changed_by_user_id                                                          as changed_by_user_id,
-    created_by_user_id                                                          as row_updated_on_utc
+    created_by_user_id                                                          as row_updated_on_utc,
+    _airbyte_raw_id                                                             as _airbyte_raw_id,
+    _airbyte_extracted_at                                                       as _airbyte_extracted_at,
+    _airbyte_generation_id                                                      as _airbyte_generation_id,
+    _airbyte_meta                                                               as _airbyte_meta
 
 FROM {{ source('bookings', 'booking') }}
