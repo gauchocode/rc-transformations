9f6f8ee36a6eefec5116f0f49bb2adf0fe83e23e
Franco Liza <fliza@gauchocode.com>
2024-10-01 16:25:44 -0300

    update booking model

--- models/turnit_new_booking/booking.sql
+++ models/turnit_new_booking/booking.sql
@@ -1,34 +1,16 @@
-{{ config(
-    materialized='incremental',
-    unique_key='id'
-) }}
-
-{# Verificamos si la columna salesforce_id existe #}
-{% set columns_in_table = adapter.get_columns_in_relation(ref('booking')) %}
+{{ config(materialized="table") }}
 
 WITH source_data AS (
     SELECT 
-        {% if 'salesforce_id' in columns_in_table %}
-            COALESCE(salesforce_id, '') as salesforce_id,
-        {% else %}
-            '' as salesforce_id,
-        {% endif %}
+        ''                                           as salesforce_id,
         id                                           as id,
         reference                                    as reference,
         changed_on_utc                               as changed_on_utc,
         changed_on_utc                               as created_on_utc,
         changed_by_user_id                           as changed_by_user_id,
         created_by_user_id                           as row_updated_on_utc,
-        _airbyte_raw_id                              as _airbyte_raw_id,
-        _airbyte_extracted_at                        as _airbyte_extracted_at,
-        _airbyte_generation_id                       as _airbyte_generation_id,
-        _airbyte_meta                                as _airbyte_meta
-    FROM {{ source('bookings', 'booking') }}
+    FROM {{ source('public', 'booking') }}
 )
 
 SELECT *
 FROM source_data
-
-{% if is_incremental() %}
-WHERE changed_on_utc > (SELECT MAX(changed_on_utc) FROM {{ this }})
-{% endif %}
