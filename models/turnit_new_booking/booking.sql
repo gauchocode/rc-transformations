2467305bc7033c705c5d1b917c8938aee6cf9f8e
Franco Liza <fliza@gauchocode.com>
2024-10-01 10:17:38 -0300

    redesing booking table

--- models/turnit_new_booking/booking.sql
+++ models/turnit_new_booking/booking.sql
@@ -1,16 +1,28 @@
-{{ config(materialized="table" ) }}
+{{ config(
+    materialized='incremental',
+    unique_key='id'
+) }}
 
-SELECT 
-    ''                                                                          as saleforce_id,
-    id                                                                          as id,
-    reference                                                                   as reference,
-    changed_on_utc                                                              as changed_on_utc,
-    changed_on_utc                                                              as created_on_utc,
-    changed_by_user_id                                                          as changed_by_user_id,
-    created_by_user_id                                                          as row_updated_on_utc,
-    _airbyte_raw_id                                                             as _airbyte_raw_id,
-    _airbyte_extracted_at                                                       as _airbyte_extracted_at,
-    _airbyte_generation_id                                                      as _airbyte_generation_id,
-    _airbyte_meta                                                               as _airbyte_meta
+WITH source_data AS (
+    SELECT 
+        COALESCE(salesforce_id, '')                  as saleforce_id,
+        id                                           as id,
+        reference                                    as reference,
+        changed_on_utc                               as changed_on_utc,
+        changed_on_utc                               as created_on_utc,
+        changed_by_user_id                           as changed_by_user_id,
+        created_by_user_id                           as row_updated_on_utc,
+        _airbyte_raw_id                              as _airbyte_raw_id,
+        _airbyte_extracted_at                        as _airbyte_extracted_at,
+        _airbyte_generation_id                       as _airbyte_generation_id,
+        _airbyte_meta                                as _airbyte_meta
+    FROM {{ source('bookings', 'booking') }}
+)
 
-FROM {{ source('bookings', 'booking') }}
+SELECT *
+FROM source_data
+
+-- Lógica incremental
+{% if is_incremental() %}
+WHERE changed_on_utc > (SELECT MAX(changed_on_utc) FROM {{ this }})
+{% endif %}
