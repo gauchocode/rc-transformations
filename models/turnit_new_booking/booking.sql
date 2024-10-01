8a7f92a8521e9f0f40c95652a6937f39550f48c2
Franco Liza <fliza@gauchocode.com>
2024-10-01 14:41:56 -0300

    update again booking model

--- models/turnit_new_booking/booking.sql
+++ models/turnit_new_booking/booking.sql
@@ -3,9 +3,12 @@
     unique_key='id'
 ) }}
 
+{# Verificamos si la columna salesforce_id existe #}
+{% set columns_in_table = adapter.get_columns_in_relation(ref('booking')) %}
+
 WITH source_data AS (
     SELECT 
-        {% if 'salesforce_id' in dbt_utils.get_column_names(source('bookings', 'booking')) %}
+        {% if 'salesforce_id' in columns_in_table %}
             COALESCE(salesforce_id, '') as salesforce_id,
         {% else %}
             '' as salesforce_id,
@@ -26,7 +29,6 @@ WITH source_data AS (
 SELECT *
 FROM source_data
 
--- Lógica incremental
 {% if is_incremental() %}
 WHERE changed_on_utc > (SELECT MAX(changed_on_utc) FROM {{ this }})
 {% endif %}
