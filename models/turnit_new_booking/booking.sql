7c324de91cc836b02e5bae8797e51431d3fb34fc
Franco Liza <fliza@gauchocode.com>
2024-10-01 17:36:00 -0300

    revert

--- models/turnit_new_booking/booking.sql
+++ models/turnit_new_booking/booking.sql
@@ -1,11 +1,16 @@
 {{ config(materialized="table") }}
 
-SELECT 
-    ''                                           as salesforce_id,
-    id                                           as id,
-    reference                                    as reference,
-    changed_on_utc                               as changed_on_utc,
-    changed_on_utc                               as created_on_utc,
-    changed_by_user_id                           as changed_by_user_id,
-    created_by_user_id                           as row_updated_on_utc,
-FROM {{ source('public', 'booking') }}
+WITH source_data AS (
+    SELECT 
+        ''                                           as salesforce_id,
+        id                                           as id,
+        reference                                    as reference,
+        changed_on_utc                               as changed_on_utc,
+        changed_on_utc                               as created_on_utc,
+        changed_by_user_id                           as changed_by_user_id,
+        created_by_user_id                           as row_updated_on_utc
+    FROM {{ source('public', 'booking') }}
+)
+
+SELECT *
+FROM source_data
